2ef809a3daa5e114b20544686ab00dd80bb00dd5
pedro5g <pedluiz321@gmail.com>
2025-04-22 16:41:16 -0300

    feat: add helmet

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client"
 import { App } from "./app.tsx"
 import { QueyProvider } from "./context/query-provider.tsx"
 import "./index.css"
+import { HelmetProvider } from "react-helmet-async"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueyProvider>
-      <App />
-    </QueyProvider>
+    <HelmetProvider>
+      <QueyProvider>
+        <App />
+      </QueyProvider>
+    </HelmetProvider>
   </StrictMode>,
 )
--- apps/web/src/pages/leading-page.tsx
+++ apps/web/src/pages/leading-page.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async"
 import { Features } from "../components/leading-page/features"
 import { Header } from "../components/leading-page/header"
 import { Hero } from "../components/leading-page/hero"
@@ -5,11 +6,14 @@ import { HowWeWork } from "../components/leading-page/how-we-work"
 
 export function LeadingPage() {
   return (
-    <div className="flex flex-col items-center justify-center w-full mx-auto">
-      <Header />
-      <Hero />
-      <HowWeWork />
-      <Features />
-    </div>
+    <>
+      <Helmet title="PAY Agent | Leading" />
+      <div className="flex flex-col items-center justify-center w-full mx-auto">
+        <Header />
+        <Hero />
+        <HowWeWork />
+        <Features />
+      </div>
+    </>
   )
 }
